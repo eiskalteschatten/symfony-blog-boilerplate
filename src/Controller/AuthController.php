518e5cbbd055aefbe5ec9354d1e612e5eb8692e9
Alex Seifert <alex@alexseifert.com>
2023-04-14 15:03:59 +0200

    Create the login form

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use App\Entity\Login;
+use App\Form\LoginType;
+
 class AuthController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
+        $login = new Login();
+        $form = $this->createForm(LoginType::class, $login);
+
         return $this->render('auth/login.html.twig', [
             'controller_name' => 'AuthController',
+            'form' => $form,
         ]);
     }
 
