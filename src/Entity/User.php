6cdd251d2ff6dd7d5777964ea1b2480aece6509a
Alex Seifert <alex@alexseifert.com>
2023-04-14 22:55:21 +0200

    Add an error when an email address is already present

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,8 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username.')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email address.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
