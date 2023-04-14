dba9f1752fa77e966701a2a73378d3b562fd6541
Alex Seifert <alex@alexseifert.com>
2023-04-14 17:05:04 +0200

    Support dark mode

--- assets/bootstrap.ts
+++ assets/bootstrap.ts
@@ -1,1 +1,7 @@
 import './styles/bootstrap.scss';
+
+(function() {
+    const handleDarkModeChange = event => document.documentElement.setAttribute('data-bs-theme', event.matches ? 'dark' : 'light');
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', handleDarkModeChange);
+    handleDarkModeChange(window.matchMedia('(prefers-color-scheme: dark)'));
+})()
