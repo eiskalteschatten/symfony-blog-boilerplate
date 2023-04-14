e6a69a24f6492961ff0f52408476e10c0ad5536f
Alex Seifert <alex@alexseifert.com>
2023-04-14 14:17:55 +0200

    Fix a naming conflict in a better way

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.ts')
-    .addEntry('bootstrap', './assets/bootstrapLib.ts')
+    .addEntry('bootstrap', './assets/bootstrap.ts')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
