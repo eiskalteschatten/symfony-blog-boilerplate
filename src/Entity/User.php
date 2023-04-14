a04719fe4fb070869833c8acaf8fdb746b111421
Alex Seifert <alex@alexseifert.com>
2023-04-14 21:28:57 +0200

    Add email and name to the user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,6 +18,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $username = null;
 
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email;
+
+    #[ORM\Column]
+    private ?string $firstName;
+
+    #[ORM\Column]
+    private ?string $lastName;
+
     #[ORM\Column]
     private array $roles = [];
 
@@ -40,6 +49,42 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUsername(string $username): self
     {
         $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
