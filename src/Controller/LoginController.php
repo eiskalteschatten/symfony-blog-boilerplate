a42d7b2a33cd36293f940a1c677cb24c53137c85
Alex Seifert <alex@alexseifert.com>
2023-04-14 22:50:54 +0200

    Cleanup

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
