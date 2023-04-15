7676dca2d91bd14aea3cc036eeed798123f42d41
Alex Seifert <alex@alexseifert.com>
2023-04-15 21:51:12 +0200

    Redirect away from the login and registration pages if the user is already logged in

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,12 +12,17 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_homepage');
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('auth/login.html.twig', [
             'controller_name' => 'LoginController',
-            'last_username' => $lastUsername
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 
